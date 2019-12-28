f0a1c947851ab4ac06e0dab3b69def357ef87499
TobyChen0106 <awesdjzo@gmail.com>
2019-12-28 22:11:15 +0800

    update

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,68 @@
 const mongoose = require('mongoose')
-const Card = require('./Card');
 const Schema = mongoose.Schema
+
+
+const setMongoMixedWithBadKeys = data =>
+    Array.isArray(data)
+        ? data.map(setMongoMixedWithBadKeys)
+        : typeof data === 'object'
+            ? Object.entries(data).reduce((a, [key, value]) => ({ ...a, [key.replace('.', '__').replace('$', '___')]: setMongoMixedWithBadKeys(value) }), {})
+            : data
+
+const getMongoMixedWithBadKeys = data =>
+    Array.isArray(data)
+        ? data.map(getMongoMixedWithBadKeys)
+        : typeof data === 'object'
+            ? Object.entries(data).reduce((a, [key, value]) => ({ ...a, [key.replace('__', '.').replace('___', '$')]: getMongoMixedWithBadKeys(value) }), {})
+            : data
+
+const Note = new Schema({
+    updateTime: {
+        type: String,
+        required: true
+    },
+    updateSource: {
+        type: String,
+        required: true
+    },
+})
+
+const CardSchema = new Schema({
+    cardID: {
+        type: Number,
+        required: true
+    },
+    cardName: {
+        type: String,
+        required: true
+    },
+    cardBank: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    imageRotate: {
+        type: Boolean,
+        required: true
+    },
+    imageLocal: {
+        type: String,
+        required: true
+    },
+    offer: {
+        type: Schema.Types.Mixed,
+        get: getMongoMixedWithBadKeys,
+        set: setMongoMixedWithBadKeys,
+        required: true
+    },
+    note: {
+        type: Note,
+        required: true
+    }
+});
 
 const UserSchema = new Schema({
     lineID: {
@@ -24,7 +86,7 @@ const UserSchema = new Schema({
         required: true
     },
     cards:{
-        type: [Card],
+        type: [CardSchema],
         default: undefined,
     }
 });
