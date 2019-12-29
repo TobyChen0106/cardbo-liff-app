c7f956503b2c948df5b0e26b21dce573ceda74cb
TobyChen0106 <awesdjzo@gmail.com>
2019-12-30 02:12:26 +0800

    update

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -108,10 +108,10 @@ class App extends Component {
             ).then((data) => {
                 console.log(data);
             }).then(() => {
-                // liff.sendMessages([{
-                //     'type': 'text',
-                //     'text': "Done!"
-                // }])
+                liff.sendMessages([{
+                    'type': 'text',
+                    'text': "Done!"
+                }])
             }).catch(function (error) {
                 window.alert("Error sending message: " + error);
             }).then(() => {
