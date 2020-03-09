b13e961e25935a9495fc6ab77ce61d502c1103fe
TobyChen0106 <awesdjzo@gmail.com>
2020-03-09 15:38:57 +0800

    update mongo atlas url

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const apiRoute = require('./src/route/api');
 
 // mongodb
 const mongoose = require('mongoose');
-mongoURL = "mongodb+srv://Toby0106:dbforcardbo@cluster0-gfwld.mongodb.net/cardbo-db?retryWrites=true&w=majority"
+const dbName = "dbCarbo"
+const usrName = "Mike"
+const usrPswd = "12345"
+mongoURL = `mongodb+srv://${usrName}:${usrPswd}@cluster0-snwtj.mongodb.net/${dbName}?retryWrites=true&w=majority`
+// mongoURL = "mongodb+srv://Toby0106:dbforcardbo@cluster0-gfwld.mongodb.net/cardbo-db?retryWrites=true&w=majority"
 mongoose.connect(mongoURL, {useNewUrlParser: true});
 db = mongoose.connection;
 db.on('error', e => {
