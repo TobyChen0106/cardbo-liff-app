5c9d1355455e9f9c3139d879a0a990307fbe7844
TobyChen0106 <awesdjzo@gmail.com>
2020-03-17 10:39:12 +0800

    liff.ready() test

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -56,8 +56,9 @@ class App extends Component {
             allCardList = data;
             // console.log(data);
         });
+        var profile;
         liff.init({ liffId: '1653324267-e53V2QWz' }, async (data) => {
-            let profile = await liff.getProfile();
+            profile = await liff.getProfile();
             if (!profile.userId) {
                 window.alert("USER ID ERROR!");
             }
@@ -65,60 +66,111 @@ class App extends Component {
                 displayName: profile.displayName,
                 userId: profile.userId
             });
-        })
+        });
+        liff.ready.then(()=>{
+            fetch('/api/check-users', {
+                method: 'POST',
+                body: JSON.stringify({ userID: profile.userId }),
+                headers: new Headers({
+                    'Content-Type': 'application/json'
+                })
+            }).catch(function (error) {
+                window.alert("[Error] " + error);
+            }).then(
+                res => res.json()
+            ).then((data) => {
+                if (data) {
+                    this.setState({ IDregistered: true });
+                    this.setState({ agreeCheck: true });
+    
+                    this.setState({ userId: data.lineID });
+                    this.setState({ nickName: data.nickName });
+                    this.setState({ age: data.age });
+                    this.setState({ gender: data.gender });
+                    this.setState({ saveOrSubmit: "Save" });
+                    const bankList = this.state.bankList.map((i, index) => (
+                        { label: i, value: index }
+                    ));
+                    var cards = [];
+                    if (data.cards.length > 0) {
+                        for (var i = 0; i < data.cards.length; ++i) {
+                            var cardIndex = allCardList.map((i, index) => (i.cardID)).indexOf(data.cards[i]);
+                            var _card = allCardList[cardIndex];
+                            var _options = allCardList.filter(card => card.bankName === _card.bankName).map((_i, _index) => (
+                                { label: _i.cardName, value: _index }
+                            ));
+                            var userCard = {
+                                bank: _card.bankName,
+                                card: _card.cardName,
+                                cardID: _card.cardID,
+                                selectedBank: { label: _card.bankName, value: bankList.indexOf(_card.bankName) },
+                                selectedCard: _options.filter(op => op.label === _card.cardName)[0],
+                                options: _options
+                            }
+                            cards.push(userCard);
+                        }
+                    } else {
+                        cards.push({ bank: '', card: '', options: [] });
+                    }
+                    this.setState({ cards: cards });
+                } else {
+                    this.setState({ IDregistered: false });
+                }
+            });
+        });
     }
     componentDidMount() {
         var allCardList = this.state.allCards;
-        fetch('/api/check-users', {
-            method: 'POST',
-            body: JSON.stringify({ userID: this.state.userId }),
-            headers: new Headers({
-                'Content-Type': 'application/json'
-            })
-        }).catch(function (error) {
-            window.alert("[Error] " + error);
-        }).then(
-            res => res.json()
-        ).then((data) => {
-            if (data) {
-                this.setState({ IDregistered: true });
-                this.setState({ agreeCheck: true });
+        // fetch('/api/check-users', {
+        //     method: 'POST',
+        //     body: JSON.stringify({ userID: this.state.userId }),
+        //     headers: new Headers({
+        //         'Content-Type': 'application/json'
+        //     })
+        // }).catch(function (error) {
+        //     window.alert("[Error] " + error);
+        // }).then(
+        //     res => res.json()
+        // ).then((data) => {
+        //     if (data) {
+        //         this.setState({ IDregistered: true });
+        //         this.setState({ agreeCheck: true });
 
-                this.setState({ userId: data.lineID });
-                this.setState({ nickName: data.nickName });
-                this.setState({ age: data.age });
-                this.setState({ gender: data.gender });
-                this.setState({ saveOrSubmit: "Save" });
-                const bankList = this.state.bankList.map((i, index) => (
-                    { label: i, value: index }
-                ));
-                var cards = [];
-                if (data.cards.length > 0) {
-                    for (var i = 0; i < data.cards.length; ++i) {
-                        var cardIndex = allCardList.map((i, index) => (i.cardID)).indexOf(data.cards[i]);
-                        var _card = allCardList[cardIndex];
-                        var _options = allCardList.filter(card => card.bankName === _card.bankName).map((_i, _index) => (
-                            { label: _i.cardName, value: _index }
-                        ));
-                        var userCard = {
-                            bank: _card.bankName,
-                            card: _card.cardName,
-                            cardID: _card.cardID,
-                            selectedBank: { label: _card.bankName, value: bankList.indexOf(_card.bankName) },
-                            selectedCard: _options.filter(op => op.label === _card.cardName)[0],
-                            options: _options
-                        }
-                        // console.log(userCard);
-                        cards.push(userCard);
-                    }
-                } else {
-                    cards.push({ bank: '', card: '', options: [] });
-                }
-                this.setState({ cards: cards });
-            } else {
-                this.setState({ IDregistered: false });
-            }
-        });
+        //         this.setState({ userId: data.lineID });
+        //         this.setState({ nickName: data.nickName });
+        //         this.setState({ age: data.age });
+        //         this.setState({ gender: data.gender });
+        //         this.setState({ saveOrSubmit: "Save" });
+        //         const bankList = this.state.bankList.map((i, index) => (
+        //             { label: i, value: index }
+        //         ));
+        //         var cards = [];
+        //         if (data.cards.length > 0) {
+        //             for (var i = 0; i < data.cards.length; ++i) {
+        //                 var cardIndex = allCardList.map((i, index) => (i.cardID)).indexOf(data.cards[i]);
+        //                 var _card = allCardList[cardIndex];
+        //                 var _options = allCardList.filter(card => card.bankName === _card.bankName).map((_i, _index) => (
+        //                     { label: _i.cardName, value: _index }
+        //                 ));
+        //                 var userCard = {
+        //                     bank: _card.bankName,
+        //                     card: _card.cardName,
+        //                     cardID: _card.cardID,
+        //                     selectedBank: { label: _card.bankName, value: bankList.indexOf(_card.bankName) },
+        //                     selectedCard: _options.filter(op => op.label === _card.cardName)[0],
+        //                     options: _options
+        //                 }
+        //                 // console.log(userCard);
+        //                 cards.push(userCard);
+        //             }
+        //         } else {
+        //             cards.push({ bank: '', card: '', options: [] });
+        //         }
+        //         this.setState({ cards: cards });
+        //     } else {
+        //         this.setState({ IDregistered: false });
+        //     }
+        // });
     }
     formOnSubmit = () => {
         if (this.state.age === 0) {
